e394e3757b6e295e5cbea4195e8246b2f1b7b3b8
LvcLaurent <lau.sion@gmail.com>
2017-07-07 17:39:47 +0200

    Contour squelette finished ....................

--- src/main/webapp/js/contour.js
+++ src/main/webapp/js/contour.js
@@ -33,10 +33,79 @@ function inverserCollectionMenu(){
 }
 
 $(document).ready(function() {
-	closeNav();
 	$('#menuCollection').hide();
 });
 
 window.onresize = function() {
-	closeNav();
 }
+
+window.addEventListener('resize', function(event){
+	if (window.innerWidth > 768)
+		//console.log(window.innerWidth);
+		$('#boutonside').css('display','none');
+	else
+		$('#boutonside').css('display','block');
+});
+	  
+$(document).ready(function(){
+	if (window.innerWidth > 768)
+		$('#boutonside').css('display','none');
+	else
+		$('#boutonside').css('display','block');
+});
+
+/**
+ * Import du code
+ */
+$(document).ready(function(){
+	var myNavBar = {
+	    flagAdd: true,
+	    elements: [],
+	    init: function (elements) {
+	        this.elements = elements;
+	    },
+	    add : function() {
+	        if(this.flagAdd) {
+	            for(var i=0; i < this.elements.length; i++) {
+	                document.getElementById(this.elements[i]).className += " fixed-theme";
+	            }
+	            this.flagAdd = false;
+	        }
+	    },
+	    remove: function() {
+	        for(var i=0; i < this.elements.length; i++) {
+	            document.getElementById(this.elements[i]).className =
+	                    document.getElementById(this.elements[i]).className.replace( /(?:^|\s)fixed-theme(?!\S)/g , '' );
+	        }
+	        this.flagAdd = true;
+	    }
+	};
+	myNavBar.init(  [
+	    "header",
+	    "header-container",
+	    "brand"
+	]);
+	function offSetManager(){
+
+	    var yOffset = 0;
+	    var currYOffSet = window.pageYOffset;
+
+	    if(yOffset < currYOffSet) {
+	        myNavBar.add();
+	    }
+	    else if(currYOffSet == yOffset){
+	        myNavBar.remove();
+	    }
+
+	}
+	window.onscroll = function(e) {
+	    offSetManager();
+	}
+	offSetManager();
+});
+
+$(document).ready(function() {
+	  $('.btn').click(function() {
+	    $('.wrap').slideToggle(400);
+	  });
+	});

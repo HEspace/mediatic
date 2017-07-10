58815ad47c3e0eaede717ff098511c27e6335337
LvcLaurent <lau.sion@gmail.com>
2017-07-10 16:31:46 +0200

    lien et assemblage des pages terminé : (CSS modifié car confict)

--- src/main/webapp/js/contour.js
+++ src/main/webapp/js/contour.js
@@ -54,7 +54,19 @@ $(document).ready(function(){
 		$('#boutonside').css('display','block');
 });
 
+function fib(a){
+	console.log(a);
+	if(a==0){
+		return 0;
+	}
+	else if(a==1){
+		return 1;
+	}
+	else
+		return fib(a-1)+fib(a-2);
+}
 
+console.log(fib(3));
 /**
  * Import du code
  */
--- src/main/webapp/js/creation.js
+++ src/main/webapp/js/creation.js
@@ -2,6 +2,21 @@
  * 
  */
 
+/*
+$(document).ready(function(){
+	if (document.getElementById("dateNaissance")== option.getAttribute("value")){
+		
+	f =	function jourDepuis(document.getElementById("dateNaissance"));
+	f.elements["age"].value = 'montexte';
+		
+		
+	}
+	else
+		return 0;
+	
+});
+*/
+
 function jourDepuis(date) {
 	var temps = (new Date()).getTime()-date.getTime();
 	return (temps/1000/60/60/24).toFixed(0);

b46193de1f639c9a95737f14010710d4eb953769
H-Espace <Elfilali.hedy@gmail.com>
2017-07-19 16:09:39 +0200

    Ajout du constructeur dans [Adherent.java] et modification de [AdherentService.java](appel de la méthode creer)

--- src/main/java/adherent/service/AdherentService.java
+++ src/main/java/adherent/service/AdherentService.java
@@ -1,25 +1,18 @@
 package adherent.service;
 
+import adherent.dao.AdherentDAO;
 import adherent.model.Adherent;
-import cotisation.model.Cotisation;
+
 
 public class AdherentService {
 	
-	public Adherent cree(String nom, String prenom, String Email, String DateNaissance,
-			String adress, String cp, String ville, Cotisation cotisation){
-		Adherent ad = new Adherent();
-		ad.setNom(nom);
-		ad.setPrenom(prenom);
-		ad.setEmail(Email);
-		ad.setDateNaissance(DateNaissance);
-		ad.setAdress(adress);
-		ad.setCp(cp);
-		ad.setVille(ville);
-		ad.setCotisation(cotisation);
-		return ad;
-
+	
+	public void creer(Adherent adh){
+		AdherentDAO adao = AdherentDAO.instance(); 
+		adao.creer(adh);
 		
 	}
+	
 	
 
 

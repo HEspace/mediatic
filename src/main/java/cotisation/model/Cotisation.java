f872a31a13c49eb3f5dc34a143282235b441700c
Husref Baltic <husref.baltic@gmail.com>
2017-07-19 12:23:03 +0200

    Ajout primary key cotisation et emprunt

--- src/main/java/cotisation/model/Cotisation.java
+++ src/main/java/cotisation/model/Cotisation.java
@@ -1,12 +1,18 @@
 package cotisation.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
 import adherent.model.Adherent;
 
 @Entity
 public class Cotisation {
+	
+	@Id
+	@GeneratedValue
+	private long id;
 	
 	@OneToOne(mappedBy = "cotisation")
 	private Adherent adherent;
--- src/main/java/emprunt/model/Emprunt.java
+++ src/main/java/emprunt/model/Emprunt.java
@@ -1,10 +1,16 @@
 package emprunt.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Emprunt {
+	
+	@Id
+	@GeneratedValue
+	private long id;
 	
 	@NotBlank
 	private long id_adherent;

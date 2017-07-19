b09d72771c4b13477174e96d9f4d02dacbc69d03
Husref Baltic <husref.baltic@gmail.com>
2017-07-19 14:18:39 +0200

    Ajout classe MediaDAO

---
+++ src/main/java/media/dao/MediaDAO.java
@@ -0,0 +1,22 @@
+package media.dao;
+
+import dao.GenericDAO;
+import media.model.Media;
+
+public class MediaDAO extends GenericDAO<Media>{
+
+	private static MediaDAO dao;
+	
+	private MediaDAO() {
+		super(Media.class);
+		
+	}
+	
+	public static MediaDAO instaceof(){
+		if(dao == null)
+			dao = new MediaDAO();
+		return dao;
+	}	
+	
+
+}

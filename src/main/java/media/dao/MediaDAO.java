57405b4450db9c3f7d39fed84b21de8b0c35b240
Husref Baltic <husref.baltic@gmail.com>
2017-07-19 16:43:10 +0200

    Ajout method listEmprunteur() pour mediaDAO

--- src/main/java/media/dao/MediaDAO.java
+++ src/main/java/media/dao/MediaDAO.java
@@ -1,5 +1,12 @@
 package media.dao;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import adherent.model.Adherent;
+import dao.DatabaseHelper;
 import dao.GenericDAO;
 import media.model.Media;
 
@@ -17,6 +24,18 @@ public class MediaDAO extends GenericDAO<Media>{
 			dao = new MediaDAO();
 		return dao;
 	}	
+	
+	public List<Adherent> listEmprunteur(Media m){
+		EntityManager em = DatabaseHelper.createEntityManager();
+		TypedQuery<Adherent> query = em.createQuery(
+				"select a " +
+				"from Media m " +
+				"inner join m.listEmprunt as le " +
+				"where le.id=:id ", Adherent.class);
+		query.setParameter("id", m.getId());
+		List<Adherent> l = query.getResultList();
+		return l;
+	}
 	
 
 }
--- src/main/java/media/service/MediaService.java
+++ src/main/java/media/service/MediaService.java
@@ -1,5 +1,8 @@
 package media.service;
 
+import java.util.List;
+
+import adherent.model.Adherent;
 import media.dao.MediaDAO;
 import media.model.Media;
 
@@ -8,5 +11,10 @@ public class MediaService {
 	public void creer(Media m){
 		MediaDAO mdao = MediaDAO.instance();
 		mdao.creer(m);
+	}
+	
+	public List<Adherent> listEmprunteur(Media m){
+		MediaDAO mdao = MediaDAO.instance();
+		return mdao.listEmprunteur(m);
 	}
 }

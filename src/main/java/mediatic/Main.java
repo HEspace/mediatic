bb27a3c738d783d12958fff7b17384076a481513
Husref Baltic <husref.baltic@gmail.com>
2017-07-19 14:25:13 +0200

    Ajout getter et setter classe modele

--- src/main/java/mediatic/Main.java
+++ src/main/java/mediatic/Main.java
@@ -3,9 +3,14 @@ package mediatic;
 import javax.persistence.EntityManager;
 
 import dao.DatabaseHelper;
+import media.model.Media;
 
 public class Main {
 	public static void main(String args[]){
 		EntityManager em = DatabaseHelper.createEntityManager();
+		
+		/**Création Médias*/
+		Media m1 = new Media();
+		
 	}
 }

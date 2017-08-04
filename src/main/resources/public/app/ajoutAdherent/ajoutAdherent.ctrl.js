16057dc65da15ef5c7884497f90e5812c94f2f80
H-Espace <Elfilali.hedy@gmail.com>
2017-08-04 11:18:04 +0200

    supression .config dans les fichiers ajoutMedia et ajoutAdherent

--- src/main/resources/public/app/ajoutAdherent/ajoutAdherent.ctrl.js
+++ src/main/resources/public/app/ajoutAdherent/ajoutAdherent.ctrl.js
@@ -2,12 +2,6 @@
 
 angular.module('mediatic.ajoutAdherent', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/ajoutAdherent', {
-    templateUrl: 'ajoutAdherent/ajoutAdherent.html',
-    controller: 'AjoutAdherentCtrl'
-  });
-}])
 
 .controller('AjoutAdherentCtrl', [function() {
 
--- src/main/resources/public/app/ajoutMedia/ajoutMedia.ctrl.js
+++ src/main/resources/public/app/ajoutMedia/ajoutMedia.ctrl.js
@@ -2,12 +2,7 @@
 
 angular.module('mediatic.ajoutMedia', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/ajoutMedia', {
-    templateUrl: 'ajoutMedia/ajoutMedia.html',
-    controller: 'AjoutMediaCtrl'
-  });
-}])
+
 
 .controller('AjoutMediaCtrl', [function() {
 

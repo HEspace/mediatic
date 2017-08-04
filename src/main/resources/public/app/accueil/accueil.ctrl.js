df2329f1e07c24489e1c608177e39f587398f3c3
Husref Baltic <husref.baltic@gmail.com>
2017-08-04 10:33:37 +0200

    Séparation pour recherche et accueil angular

---
+++ src/main/resources/public/app/accueil/accueil.ctrl.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('mediatic.accueil', ['ngRoute'])
+.controller('AccueilCtrl', [function() {
+
+}]);
---
+++ src/main/resources/public/app/recherche/recherche.ctrl.js
@@ -0,0 +1,5 @@
+'use strict';
+angular.module('mediatic.recherche', ['ngRoute'])
+.controller('RechercheCtrl', [function(){
+
+}]);

583ece19c338f72aa9dea48bc352999d157c34d1
H-Espace <Elfilali.hedy@gmail.com>
2017-08-04 17:33:00 +0200

    Modifications mineures

--- src/main/resources/public/app/mediatic.js
+++ src/main/resources/public/app/mediatic.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('mediatic', [
   'ngRoute',
+  'ngMessages',
   'mediatic.recherche',
   'mediatic.accueil',
   'mediatic.ajoutMedia',

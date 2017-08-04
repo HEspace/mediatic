564cbf98a9092ed449e83f8b041eebfbd36e1966
Husref Baltic <husref.baltic@gmail.com>
2017-08-04 12:23:26 +0200

    ajout fonctionnalité button adh/media

--- src/main/resources/public/app/recherche/recherche.ctrl.js
+++ src/main/resources/public/app/recherche/recherche.ctrl.js
@@ -2,55 +2,60 @@
 
 angular.module('mediatic.recherche', ['ngRoute'])
 
-.controller('RechercheCtrl', [function(){
+    .controller('RechercheCtrl', ['$scope', function ($scope) {
 
-    function getval(sel){
-	var index = document.getElementById("sel").selectedIndex;
-	var options = document.getElementById("sel").options;
-	if(options[index].text == "Médias"){
-		$("#radioAdhe").hide();
-		$("#checkMedia").fadeIn();
-		$("#tabAdherent").hide();
-		$("#tabMedia").fadeIn();
-	}else{
-		console.log("adherent");
-		$("#checkMedia").hide();
-		$("#radioAdhe").fadeIn();
-		$("#tabAdherent").fadeIn();
-		$("#tabMedia").hide();
-	}
-}
+        $scope.getval = function getval(sel) {
+            var index = document.getElementById("sel").selectedIndex;
+            var options = document.getElementById("sel").options;
+            if (options[index].text == "Médias") {
+                $("#radioAdhe").hide();
+                $("#checkMedia").fadeIn();
+                $("#tabAdherent").hide();
+                $("#tabMedia").fadeIn();
+            } else {
+                console.log("adherent");
+                $("#checkMedia").hide();
+                $("#radioAdhe").fadeIn();
+                $("#tabAdherent").fadeIn();
+                $("#tabMedia").hide();
+            }
+        }
+        
+        $('.selectpicker').selectpicker({
+            style: 'btn-primary btn-lg',
+            width: 'fit'
+        });
 
 
-$(document).ready(function () {
-	getval(document.getElementById("sel"));
-	//drawTab();
-	$("#radioAdhe").hide();
-	$("#tabAdherent").hide();
-	//$(".rowHidden").hide();
-	//$("div.divHidden1").hide();
+        $(document).ready(function () {
+            getval(document.getElementById("sel"));
+            //drawTab();
+            $("#radioAdhe").hide();
+            $("#tabAdherent").hide();
+            //$(".rowHidden").hide();
+            //$("div.divHidden1").hide();
 
-	$('.rowTab').click(function(){
-		//console.log("test");
-		if($(".divHiddenMedia").is(":hidden")){
-			$(".trHiddenMedia").fadeIn("slow")
-			$( ".divHiddenMedia" ).slideDown("slow");
-		}else{
-			$(".trHiddenMedia").hide("fast")
-			$(".divHiddenMedia").slideUp("fast");
-		}
+            $('.rowTab').click(function () {
+                //console.log("test");
+                if ($(".divHiddenMedia").is(":hidden")) {
+                    $(".trHiddenMedia").fadeIn("slow")
+                    $(".divHiddenMedia").slideDown("slow");
+                } else {
+                    $(".trHiddenMedia").hide("fast")
+                    $(".divHiddenMedia").slideUp("fast");
+                }
 
-		if($(".divHiddenUser").is(":hidden")){
-			$(".trHiddenUser").fadeIn("slow")
-			$( ".divHiddenUser" ).slideDown("slow");
-		}else{
-			$(".trHiddenUser").hide("fast")
-			$(".divHiddenUser").slideUp("fast");
-		}
-	});
+                if ($(".divHiddenUser").is(":hidden")) {
+                    $(".trHiddenUser").fadeIn("slow")
+                    $(".divHiddenUser").slideDown("slow");
+                } else {
+                    $(".trHiddenUser").hide("fast")
+                    $(".divHiddenUser").slideUp("fast");
+                }
+            });
 
-	$('.rowTab').hover(function () {
-		$(this).css('cursor', 'pointer');
-	});
-});
-}]);
+            $('.rowTab').hover(function () {
+                $(this).css('cursor', 'pointer');
+            });
+        });
+    }]);

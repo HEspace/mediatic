c1f1927d41f07b9573474570a9b68ba4a3541f45
Husref Baltic <husref.baltic@gmail.com>
2017-08-04 10:45:25 +0200

    Ajout controller/config pour recherche et accueil

--- src/main/resources/public/app/accueil/accueil.ctrl.js
+++ src/main/resources/public/app/accueil/accueil.ctrl.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('mediatic.accueil', ['ngRoute'])
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/accueil', {
+    templateUrl: 'accueil/accueil.html',
+    controller: 'AccueilCtrl'
+  });
+}])
 .controller('AccueilCtrl', [function() {
 
 }]);
--- src/main/resources/public/app/recherche/recherche.ctrl.js
+++ src/main/resources/public/app/recherche/recherche.ctrl.js
@@ -1,5 +1,11 @@
 'use strict';
 angular.module('mediatic.recherche', ['ngRoute'])
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/recherche', {
+    templateUrl: 'recherche/recherche.html',
+    controller: 'RechercheCtrl'
+  });
+}])
 .controller('RechercheCtrl', [function(){
 
 }]);

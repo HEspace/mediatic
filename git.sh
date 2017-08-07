6688ffa5551469caa9c29cccca53d1d654f12260
LvcLaurent <lau.sion@gmail.com>
2017-08-07 10:40:52 +0200

    test du script

---
+++ git.sh
@@ -0,0 +1,23 @@
+@echo git pull et add !
+
+git pull
+
+git add src/main/resources/public/app/ajoutMedia/*
+
+git add src/main/resources/public/app/ajoutAdherent/*
+
+git add src/main/resources/public/app/accueil/*
+
+git add src/main/resources/public/app/recherche/*
+
+git add src/main/resources/public/app/css/*
+
+git add src/main/resources/public/app/Image/*
+
+git add src/main/resources/public/app/js/*
+
+git add src/main/resources/public/app/mediatic.js
+
+git add src/main/resources/public/app/mediatic.css
+
+git add src/main/resources/public/app/index.html

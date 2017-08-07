9dfb0b7a290cf2c4a810693818bc34ee4e37f54b
LvcLaurent <lau.sion@gmail.com>
2017-08-07 18:56:20 +0200

    gestion des erreur pour le script

--- git.sh
+++ git.sh
@@ -1,8 +1,26 @@
+erreur1(){
+echo "Erreur sur le pull"
+exit 1
+}
+
+erreur2(){
+echo "Erreur sur le push"
+exit 1
+}
+
+parfait(){
+echo "Aucune Erreur"
+exit 0
+}
+
 echo "======================================================"
 echo "====================== git pull ======================"
 echo "======================================================"
 git pull
-
+if [ "$?" != 0 ];then
+    erreur1
+fi
+sleep 2
 
 echo "======================================================"
 echo "====================== git add ======================="
@@ -21,13 +39,30 @@ git add src/main/resources/public/app/js/*
 git add src/main/resources/public/app/mediatic.js
 git add src/main/resources/public/app/mediatic.css
 git add src/main/resources/public/app/index.html
+git add git.sh
+sleep 2
 
 echo "======================================================"
 echo "=================== git commit -m ===================="
 echo "======================================================"
-git commit -m "$1"
+if [ "$1" != "" ];then
+    git commit -m "$1"
+else
+    echo -n "Votre message pour le commit : "
+    read message
+    git commit -m "$message"
+fi
+sleep 2
 
 echo "======================================================"
 echo "===================== git push ======================="
 echo "======================================================"
 git push origin master
+
+if [ "$?" != 0 ];then
+    erreur2
+else
+    parfait
+fi
+
+
